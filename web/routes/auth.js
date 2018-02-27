509843ef5483027e97e81b64777df8b5b3364aed
Jon Collette <jon.collette@altopa.com>
2018-02-27 13:26:03 -0800

    adding select_account setting to google auth so it alwasys asks you to choose your account on login

--- web/routes/auth.js
+++ web/routes/auth.js
@@ -4,11 +4,18 @@ import passport from 'passport';
 export const router = new Router();
 
 router.get('/google',
-  passport.authenticate('google', { scope: ['profile'], session: false })
+  passport.authenticate('google', {
+    prompt: 'select_account',
+    scope: ['profile'],
+    session: false,
+  })
 );
 
 router.get('/google/callback',
-  passport.authenticate('google', { failureRedirect: '/auth/google', session: false }),
+  passport.authenticate('google', {
+    failureRedirect: '/auth/google',
+    session: false,
+  }),
   // this redirect will be handled in app by Linking.addEventListener
   (req, res) => res.redirect('todoapp://login?token=' + JSON.stringify(req.user.token))
 );
